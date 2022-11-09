a60e0666a92f4315845ef5d854b4ce28e7d730a3
Tracy Chen <tracy200e@gmail.com>
2022-11-09 17:45:12 +1100

    Created JavaScript file and initial cards objects

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+    {
+        "name": "Yosemite Valley",
+        "link": URL("../images/yosemite.jpg")
+    },
+    {
+        "name": "Lake Louise",
+        "link": URL("../images/lake-louise.jpg")
+    },
+    {
+        "name": "Bald Mountains",
+        "link": URL("../images/bald-mountains.jpg")
+    },
+    {
+        "name": "Latemar",
+        "link": URL("../images/latemar.jpg")
+    },
+    {
+        "name": "Vanoise National Park",
+        "link": URL("../images/vanoise.jpg")
+    },
+    {
+        "name": "Lago di Braies",
+        "link": URL("../images/lago.jpg")
+    },
+]
