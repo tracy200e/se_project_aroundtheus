2981799a68d5ce0d003c26edf22eb3acca012022
Tracy Chen <tracy200e@gmail.com>
2022-11-09 17:53:08 +1100

    Corrected object names

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 let initialCards = [
     {
-        "name": "Yosemite Valley",
-        "link": "../images/yosemite-valley.png"
+        name: "Yosemite Valley",
+        link: "../images/yosemite-valley.png"
     },
     {
-        "name": "Lake Louise",
-        "link": "../images/lake-louise.png"
+        name: "Lake Louise",
+        link: "../images/lake-louise.png"
     },
     {
-        "name": "Bald Mountains",
-        "link": "../images/bald-mountains.png"
+        name: "Bald Mountains",
+        link: "../images/bald-mountains.png"
     },
     {
-        "name": "Latemar",
-        "link": "../images/latemar.png"
+        name: "Latemar",
+        link: "../images/latemar.png"
     },
     {
-        "name": "Vanoise National Park",
-        "link": "../images/vanoise-national-park.png"
+        name: "Vanoise National Park",
+        link: "../images/vanoise-national-park.png"
     },
     {
-        "name": "Lago di Braies",
-        "link": "../images/lago-di-braise.png"
+        name: "Lago di Braies",
+        link: "../images/lago-di-braise.png"
     }
 ]
