831b20fd98663ecd31d5bc59382e760267cc815a
Tracy Chen <tracy200e@gmail.com>
2022-11-16 09:46:16 +1100

    Added code to open modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,22 @@ let initialCards = [
         link: "../images/lago-di-braise.png"
     }
 ]
+
+// Identify edit button as an element
+const editButton = document.querySelector('.profile__edit-button');
+
+// // Identify the modal as an element
+// let modal = document.querySelector('.modal');
+
+// // Add class to open modal
+// function renderModal() {
+//     if (modal.classList.contains('modal_opened') != true) {
+//         modal.classList.add('modal_opened');
+//     };
+// };
+
+// Listen for 'click' events on the Edit Button
+editButton.addEventListener("click", function(e) {
+    const eventTarget = e.target;
+    eventTarget.classList.add('modal_opened');
+});
