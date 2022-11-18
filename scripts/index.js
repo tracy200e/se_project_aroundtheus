620fb235f669ff0c4fb4886c6b86ec8a9c6eef01
Tracy Chen <tracy200e@gmail.com>
2022-11-18 20:44:56 +1100

    Switched the cards order around

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 // Create an initial list of cards
 let initialCards = [
     {
-        name: "Yosemite Valley",
-        link: "../images/yosemite-valley.png"
-    },
-    {
-        name: "Lake Louise",
-        link: "../images/lake-louise.png"
-    },
-    {
-        name: "Bald Mountains",
-        link: "../images/bald-mountains.png"
-    },
-    {
-        name: "Latemar",
-        link: "../images/latemar.png"
+        name: "Lago di Braies",
+        link: "../images/lago-di-braies.png"
     },
     {
         name: "Vanoise National Park",
         link: "../images/vanoise-national-park.png"
     },
     {
-        name: "Lago di Braies",
-        link: "../images/lago-di-braies.png"
+        name: "Lake Louise",
+        link: "../images/lake-louise.png"
+    },
+    {
+        name: "Latemar",
+        link: "../images/latemar.png"
+    },
+    {
+        name: "Bald Mountains",
+        link: "../images/bald-mountains.png"
+    },
+    {
+        name: "Yosemite Valley",
+        link: "../images/yosemite-valley.png"
     }
 ]
 
