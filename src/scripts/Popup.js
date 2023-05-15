7da6d679e7ef9c85fbe58b0878ac1544299a91a5
Tracy Chen <tracy200e@gmail.com>
2023-05-15 16:47:31 +1000

    build: created a section class as part of refactoring

---
+++ src/scripts/Popup.js
@@ -0,0 +1,21 @@
+export default class Popup {
+    constructor({ popupSelector}) {
+        this._popupElement = document.querySelector(popupSelector);
+    }
+
+    open() {
+        // opens popup
+    }
+
+    close() {
+        // closes popup
+    }
+
+    _handleEscClose() {
+        // listens for esc button
+    }
+
+    setEventListeners() {
+        // sets event listeners
+    }
+}
